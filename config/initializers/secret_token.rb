fa8cb0452181b0f6620c363a8e8e917e57f2a2e1
Paul <pmazak2002@yahoo.com>
2011-11-21 22:19:48 -0500

    init cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motivateme::Application.config.secret_token = 'f3ed463eeb4f620ed153de665c2a7d93000df3b8063f40b72bdc4c7cb39120c355e37a80e00a62e6e70ed076336acbbd862bb589f0d34a2d4af0525a0015adf1'
